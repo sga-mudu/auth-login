01781a0892222d739fb949890d74029462ab8b21
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-09-03 17:41:01 +0800

    depoy to vercel

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base:"/auth-login"
+  base: process.env.VITE_BASE_PATH ||"/auth-login"
 })
